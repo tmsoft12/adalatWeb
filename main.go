cd46298f9b4390ccae52f2c15f5b071b9a4bff70
Your Name <tmsoft12@gmail.com>
2024-12-24 14:27:19 +0500

    cors

--- main.go
+++ main.go
@@ -6,16 +6,20 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
-	// Maglumat bazasyna baglanyşmak
 	database.ConnectDB()
 
 	// Fiber app döretmek
 	app := fiber.New()
 	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "216.150.14.12", // İzin verilen IP adresi
+		AllowHeaders: "*",             // Tüm header'lara izin veriliyor
+	}))
 
 	// IP adresini almak
 	ip := os.Getenv("BASE_URL")
